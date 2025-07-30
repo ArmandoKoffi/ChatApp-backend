ffb200498f49e6781b0e3853aa8709cadc343710
ArmandoKoffi <armandokoffikoffi@gmail.com>
2025-07-30 13:09:11 +0000

    Le backend devrait maintenant démarrer et fonctionner sans cette erreur sur les favoris.

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -4,10 +4,10 @@ const bookmarkController = require("../controllers/bookmarkController");
 const auth = require("../middleware/auth");
 
 // Ajouter un favori
-router.post("/", auth, bookmarkController.addBookmark);
+router.post("/", auth.protect, bookmarkController.addBookmark);
 // Supprimer un favori
-router.delete("/:id", auth, bookmarkController.removeBookmark);
+router.delete("/:id", auth.protect, bookmarkController.removeBookmark);
 // Lister les favoris
-router.get("/", auth, bookmarkController.getBookmarks);
+router.get("/", auth.protect, bookmarkController.getBookmarks);
 
 module.exports = router;
